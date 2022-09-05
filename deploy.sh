25abca51ca4e8fdd13ed67f985a6c946551fd38a
Hector Padilla <hpadilla6201@gmail.com>
2022-09-05 15:06:46 -0400

    fixed texta color in contact page

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ echo "Removing contents from s3"
 aws s3 rm s3://hectorwebapp --recursive --exclude=""
 echo "Pushing contents to s3"
 aws s3 sync ./build s3://hectorwebapp --cache-control max-age=31536000
+
+aws cloudfront create-invalidation --distribution-id E2OVEMRAD1PL87 --paths "/*"
 exit 0
-np
